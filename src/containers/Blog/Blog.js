14c7ee448f5fa61c397bad5052c748fc1b9b9812
Carlos Aguilera <a.carlosing@gmail.com>
2018-04-24 19:43:24 -0500

    Creating a http request to get data and rendering to the screen.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                this.setState({ posts: response.data });
+            });
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} />;
+        });
+
         return (
             <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
+                <section className="Posts">{posts}</section>
                 <section>
                     <FullPost />
                 </section>
