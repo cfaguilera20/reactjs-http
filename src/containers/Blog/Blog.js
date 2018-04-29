01cd33a6a3ff91ad8e858a10390d56dbdc58fe0c
Carlos Aguilera <a.carlosing@gmail.com>
2018-04-29 07:48:44 -0500

    Creating dynamic nested routes.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,6 @@ import { Route, NavLink, Switch } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
     render() {
@@ -15,14 +14,14 @@ class Blog extends Component {
                         <ul>
                             <li>
                                 <NavLink
-                                    to="/"
+                                    to="/posts/"
                                     exact
                                     activeClassName="active"
                                     activeStyle={{
                                         textDecoration: "underline"
                                     }}
                                 >
-                                    Home
+                                    Posts
                                 </NavLink>
                             </li>
                             <li>
@@ -41,10 +40,9 @@ class Blog extends Component {
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} />*/}
-                <Route path="/" exact component={Posts} />
                 <Switch>
                     <Route path="/new-post" exact component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    <Route path="/posts/" component={Posts} />
                 </Switch>
             </div>
         );
